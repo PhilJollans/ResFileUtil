ee71aaea7b6fdb2a3d1d40478d6da9cbd19056cf
PhilJollans <phil@jollans.com>
2021-12-14 23:39:48 +0100

    Add property ResIds, remove some commented out code.

--- ResFileUtil/ResStrings.cs
+++ ResFileUtil/ResStrings.cs
@@ -15,32 +15,7 @@ namespace ResFileUtil
     public void         SortByCombinedID()                              => this.Sort ( (x,y) => x.CombinedID.CompareTo(y.CombinedID ) ) ;
     public bool         Exists ( UInt32 LocaleId, UInt32 ResId )        => this.Any ( x => ( x.ResId == ResId ) && ( x.LocaleId == LocaleId ) ) ;
     public List<UInt32> Locales                                         => this.Select ( x => x.LocaleId ).Distinct().ToList() ;
+    public List<UInt32> ResIds                                          => this.Select ( x => x.ResId ).Distinct().OrderBy(x => x).ToList() ;
     public ResString    GetByLocaleAndResId ( int LocaleId, int ResId ) => this.FirstOrDefault ( x => ( x.LocaleId == LocaleId ) && ( x.ResId == ResId ) );
-
-#if false
-    public void SortByID()
-    {
-      this.Sort ( (x,y) => x.ResId.CompareTo(y.ResId) ) ;
-    }
-
-    public bool Exists ( int ResId )
-    {
-      return this.Any ( x => x.ResId == ResId ) ;
-    }
-
-    public List<int> Locales
-    {
-      get
-      {
-        return this.Select ( x => x.LocaleId ).Distinct().ToList() ;
-      }
-    }
-
-    public ResString GetByLocaleAndResId ( int LocaleId, int ResId )
-    {
-      return this.FirstOrDefault ( x => ( x.LocaleId == LocaleId ) && ( x.ResId == ResId ) );
-    }
-#endif
-
   }
 }
